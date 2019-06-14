26950055d80fe77cb46578cd740b381990814090
Bo Wang <33285888+FrancoisMoreau@users.noreply.github.com>
2019-06-14 20:32:45 +0800

    Create StringtoInteger.cpp

---
+++ 1-100/StringtoInteger.cpp
@@ -0,0 +1,14 @@
+int string_to_integer(std::string str) {
+    auto first = str.find_first_of("+-0123456789");
+    if (first) {
+        if (str.substr(0, first).find_first_not_of(" ") != std::string::npos)
+            return 0;
+    }
+    long long res = std::stoll(str.substr(first));
+    if (res < INT32_MIN)
+        return INT32_MIN;
+    else if (res > INT32_MAX)
+        return  INT32_MAX;
+    else
+        return res;
+}

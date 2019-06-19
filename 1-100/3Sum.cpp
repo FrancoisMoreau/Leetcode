63407b896aed55985814a9c0963f17f2b257b23d
Bo Wang <33285888+FrancoisMoreau@users.noreply.github.com>
2019-06-19 14:21:15 +0800

    Create 3Sum.cpp

---
+++ 1-100/3Sum.cpp
@@ -0,0 +1,24 @@
+vector<vector<int>> threeSum(vector<int>& nums) {
+    vector<vector<int>> res;
+    if (nums.size() < 3)
+        return res;
+    std::sort(nums.begin(), nums.end());
+    for (auto it0 = nums.cbegin(); it0 != nums.cend() - 3; ++it0) {
+        auto it1 = it0 + 1;
+        auto it2 = nums.cend() - 1;
+        while (it1 < it2) {
+            if (*it1 + *it2 + *it0 < 0)
+                ++it1;
+            else if (*it1 + *it2 + *it0 > 0)
+                --it2;
+            else {
+                res.push_back({*it0, *it1, *it2});
+                ++it1;
+                --it2;
+            }
+        }
+        while (*it0 == *(it0 + 1) && it0 != nums.cend() -1)
+            ++it0;
+    }
+    return res;
+}

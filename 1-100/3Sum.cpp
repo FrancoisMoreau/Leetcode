097c19a9789ed7276eda7b6bf51cc41635c92311
Bo Wang <33285888+FrancoisMoreau@users.noreply.github.com>
2019-06-29 18:41:06 +0800

    Update 3Sum.cpp

--- 1-100/3Sum.cpp
+++ 1-100/3Sum.cpp
@@ -1,24 +1,32 @@
+
+//自己写的有bug，　参考了别人的实现。　效率最高了吧。　具体情况看豆瓣
+
 vector<vector<int>> threeSum(vector<int>& nums) {
     vector<vector<int>> res;
     if (nums.size() < 3)
         return res;
     std::sort(nums.begin(), nums.end());
-    for (auto it0 = nums.cbegin(); it0 != nums.cend() - 3; ++it0) {
+    for (auto it0 = nums.cbegin(); it0 < nums.cend() - 2; ++it0) {
         auto it1 = it0 + 1;
         auto it2 = nums.cend() - 1;
         while (it1 < it2) {
-            if (*it1 + *it2 + *it0 < 0)
+            if (*it1 + *it2 + *it0 < 0) {
                 ++it1;
-            else if (*it1 + *it2 + *it0 > 0)
+            }
+            else if (*it1 + *it2 + *it0 > 0) {
                 --it2;
+            }
             else {
                 res.push_back({*it0, *it1, *it2});
                 ++it1;
                 --it2;
+                while (*it1 == *(it1 -1) && *(it2 + 1) == *it2 && it1 < it2)
+                    ++it1;
             }
         }
-        while (*it0 == *(it0 + 1) && it0 != nums.cend() -1)
+        while (*it0 == *(it0 + 1) && it0 < nums.cend() -2)
             ++it0;
     }
     return res;
 }
+

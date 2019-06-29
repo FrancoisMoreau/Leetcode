83d04df2130faaf8ad0da4d617417a0320b1ca55
Bo Wang <33285888+FrancoisMoreau@users.noreply.github.com>
2019-06-29 18:44:38 +0800

    Update StringtoInteger.cpp

--- 1-100/StringtoInteger.cpp
+++ 1-100/StringtoInteger.cpp
@@ -13,3 +13,29 @@ int string_to_integer(std::string str) {
         return res;
 }
 有很多ｂｕｇ，　待update
+
+//自己写的很多问题，　情况太多了。　
+int myAtoi(string str) {
+        int num = 0;
+        int sign = 1;
+        const int n = str.length();
+        int i = 0;
+        while (str[i] == ' ' && i < n) i++;
+        if (str[i] == '+') {
+            i++;
+        } else if (str[i] == '-') {
+            sign = -1;
+            i++;
+        }
+        for (; i < n; i++) {
+            if (str[i] < '0' || str[i] > '9')
+                break;
+            if (num > INT32_MAX / 10 ||
+                (num == INT32_MAX/10 &&(str[i] - '0') > INT32_MAX %10)) {
+                return sign == -1 ? INT32_MIN : INT32_MAX;
+            }
+            printf("num: %d\n", num);
+            num = num * 10 + (str[i] - '0');
+        }
+        return num * sign;
+}

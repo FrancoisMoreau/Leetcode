797c42167fe8b3ee287ef6da82facc46d0af9d2d
brayden <wangbo1995ee@163.com>
2020-05-29 11:39:54 -0500

    added the solution for 612

--- 601-650/problem621.cpp
+++ 601-650/problem621.cpp
@@ -3,3 +3,66 @@
 //
 
 #include "problem621.h"
+
+#include <unordered_map>
+#include <queue>
+#include <cstdlib>
+
+using std::unordered_map;
+using std::vector;
+using std::priority_queue;
+using std::pair;
+
+// I tried to solve this problem by priority queue, but I found it too complex, my thought was wrong
+//  if we can get how many idles we need to arrange A, we will also get number of idles needed to arrange all tasks
+// please analyze this kind of problem next time on script paper !
+
+int leastInterval2(vector<char>& tasks, int n) {
+    vector<int> counter(26);
+    int max = 0;
+    int maxcount = 0;
+    for (char c : tasks) {
+        ++counter[c - 'A'];
+        if (max == counter[c - 'A']) {
+            ++maxcount;
+        } else if (max < counter[c - 'A']) {
+            max = counter[c - 'A'];
+            maxcount = 1;
+        }
+    }
+    int partcount = max - 1;
+    int partLength = n - (maxcount - 1);
+    int emptySlots = partcount * partLength;
+    int availableTasks = tasks.size() - max * maxcount;
+    int idles = std::max(0, emptySlots - availableTasks);
+    return tasks.size() + idles;
+}
+// (c[25] - 1) * (n + 1) + 25 - i  is frame size
+// when inserting chars, the frame might be "burst", then tasks.length takes precedence
+// when 25 - i > n, the frame is already full at construction, the following is still valid.
+
+int leastInterval(vector<char> &tasks, int n) {
+    vector<int> counter(26);
+    for (char c : tasks)
+        ++counter[c - 'A'];
+    std::sort(counter.begin(), counter.end());
+
+    int i = 25;
+    while (i >= 0 && counter[i] == counter[25]) --i;
+    return std::max(int(tasks.size()), (counter[25] - 1) * (n + 1) + 25 - i);
+}
+
+// similar idea
+int leastInterval3(vector<char>& tasks, int n) {
+    unordered_map<char,int>mp;
+    int count = 0;
+    for(auto e : tasks)
+    {
+        mp[e]++;
+        count = std::max(count, mp[e]);
+    }
+
+    int ans = (count-1)*(n+1);
+    for(auto e : mp) if(e.second == count) ans++;
+    return std::max((int)tasks.size(), ans);
+}
--- 601-650/problem621.h
+++ 601-650/problem621.h
@@ -5,12 +5,8 @@
 #ifndef INC_601_650_PROBLEM621_H
 #define INC_601_650_PROBLEM621_H
 
+#include <vector>
 
-
-class problem621 {
-
-};
-
-
+int leastInterval(std::vector<char>& tasks, int n);
 
 #endif //INC_601_650_PROBLEM621_H
